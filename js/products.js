91cda8b263773597e3184edd9cedd5aaa4342d07
ignaciodorrego <ignadorrego@gmail.com>
2023-08-25 18:00:10 -0300

    Diseños de Listado de Productos e Icono de pestaña

--- js/products.js
+++ js/products.js
@@ -16,18 +16,21 @@ fetch(apiUrl)
             let productosHTML = "";
             autosProductos.forEach((producto) => {
                 productosHTML += `
-                        <div class="autos-tarjetas col-md-4">
-                            <div class="card mb-4 shadow-sm custom-card">
-                                <img class="bd-placeholder-img card-img-top" src="${producto.image}" alt="${producto.name}">
-                                <h3 class="m-3">${producto.name}</h3>
-                                <div class="card-body">
-                                    <p class="card-text"><strong>Precio:</strong> ${producto.cost} ${producto.currency}</p>
-                                    <p class="card-text">${producto.description}</p>
-                                    <p class="card-text"><strong>Cantidad vendidos:</strong> ${producto.soldCount}</p>
+                        <div class="TarjetaProducto">
+                            <div class="InfoProducto"> 
+                                <img class="ImagenProducto" src="${producto.image}" alt="${producto.name}">
+                                <div class="Descripcion">
+                                    <h3>${producto.name}</h3>
+                                    <p class="descripcionProducto">${producto.description}</p>
+                                    <p class="VentasProducto"><span>Cantidad vendidos:</span> ${producto.soldCount}</p>
                                 </div>
                             </div>
+                            <div class="ComprarProducto">
+                                <p class="descripcionPrecio"><span>Precio:</span> ${producto.cost} ${producto.currency}</p>
+                                <button class="botonComprar">Comprar</button>
+                            </div>
                         </div>
-                    `;
+                `;
     });
 
     productosContainer.innerHTML = productosHTML;

87aa97247ff3f668b7c2885745c1721563d8fe29
dreyeragustin <dreyeragustin@gmail.com>
2023-11-23 08:38:29 -0300

    ordenar y limpiar código

--- js/index.js
+++ js/index.js
@@ -1,32 +1,31 @@
 // Evento que se ejecuta cuando el documento HTML se ha cargado completamente
-document.addEventListener("DOMContentLoaded", function(){
-    // Event listeners para los elementos con ID "autos", "juguetes" y "muebles"
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
-    let mmail = localStorage.getItem('mail');
-    let spanM = document.getElementById('mailNB');
-    if (mmail != null)
-    {
+document.addEventListener("DOMContentLoaded", function () {
+  // Event listeners para los elementos con ID "autos", "juguetes" y "muebles"
+  document.getElementById("autos").addEventListener("click", function () {
+    localStorage.setItem("catID", 101);
+    window.location = "products.html"
+  });
+  document.getElementById("juguetes").addEventListener("click", function () {
+    localStorage.setItem("catID", 102);
+    window.location = "products.html"
+  });
+  document.getElementById("muebles").addEventListener("click", function () {
+    localStorage.setItem("catID", 103);
+    window.location = "products.html"
+  });
+  let mmail = localStorage.getItem('mail');
+  let spanM = document.getElementById('mailNB');
+  if (mmail != null) {
     spanM.innerHTML = `${mmail}`;
-    }
+  }
 });
 // Evento que se ejecuta cuando el documento HTML se ha cargado completamente
-document.addEventListener("DOMContentLoaded",function(){
-    if(localStorage.getItem('registro') != 'true'){
-        // Si no se ha registrado (basado en el valor en el almacenamiento local), mostrar una alerta y redirigir a "login.html"
-        alert("Usted no ha iniciado sesión.")
-        window.location.href = 'login.html'
-    }
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem('registro') != 'true') {
+    // Si no se ha registrado (basado en el valor en el almacenamiento local), mostrar una alerta y redirigir a "login.html"
+    alert("Usted no ha iniciado sesión.")
+    window.location.href = 'login.html'
+  }
 });
 
 
@@ -55,7 +54,7 @@ const modoOscuroGuardado = localStorage.getItem('modo-oscuro');
 if (modoOscuroGuardado === 'true') {
   body.classList.add('modo-oscuro');
   main[0].classList.remove('bg-light')
-  
+
 } else {
   body.classList.remove('modo-oscuro');
 }
